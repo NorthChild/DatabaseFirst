34a3a8516eb1474b707d6989f502faff650887c3
michael carini <snownkc@icloud.com>
2021-08-09 16:56:02 +0100

    added new repo

---
+++ DatabaseFirstEF/DatabaseFirstEF/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DatabaseFirstEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+        }
+    }
+}
