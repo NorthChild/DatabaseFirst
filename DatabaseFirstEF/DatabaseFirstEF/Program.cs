8a64cdd5e4eb417466175785bb088bb53bb295e2
michael carini <snownkc@icloud.com>
2021-08-10 11:45:08 +0100

    added database first repo update

--- DatabaseFirstEF/DatabaseFirstEF/Program.cs
+++ DatabaseFirstEF/DatabaseFirstEF/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace DatabaseFirstEF
 {
@@ -6,8 +7,61 @@ namespace DatabaseFirstEF
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using (var db = new NorthwindContext()) 
+            {
+                Console.WriteLine(db.ContextId);
+
+
+                //foreach (var c in db.Customers) 
+                //{
+                //    Console.WriteLine($"Customer {c.ContactName} had ID {c.CustomerId} and lives in {c.City}");
+                //    Console.WriteLine(c);
+                //}
+
+                //db.Customers.ToList().ForEach(c => Console.WriteLine(c));
+
+                // ADD 
+
+                //var newCustomer = new Customer
+                //{
+                //    CustomerId = "BLOGG",
+                //    ContactName = "Joe Bloggs",
+                //    CompanyName = "ToysRme"
+                //};
+
+                //db.Customers.Add(newCustomer);
+
+                // UPDATE
+
+                //var selectedCustomer = db.Customers.Where(c => c.CustomerId == "BLOGG").FirstOrDefault();
+                //selectedCustomer.City = "New Paris";
+
+
+                // DELETE
+
+                var deletedCustomer = db.Customers.Where(c => c.CustomerId == "BLOGG").FirstOrDefault();
+
+                db.Customers.Remove(deletedCustomer);
+
+                db.SaveChanges();
+
+                var p = db.Customers.Find("BLOGG");
+
+            }
+            
+
 
         }
+    }
+
+
+    public partial class Customer
+    {
+
+        public override string ToString()
+        {
+            return $"{CustomerId} - {ContactName} - {City}";
+        }
+
     }
 }
